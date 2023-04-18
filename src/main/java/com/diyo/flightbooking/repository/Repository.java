f8f76c3ddbe6df7fe2ff9099a2e1619018771765
Rupesh <rxkst244@mail.rmu.edu>
2023-04-18 11:18:50 -0400

    Repository created

---
+++ src/main/java/com/diyo/flightbooking/repository/Repository.java
@@ -0,0 +1,7 @@
+/*
+package com.diyo.flightbooking.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@org.springframework.stereotype.Repository
+public interface Repository extends JpaRepository<Passenger, Long> {}*/
